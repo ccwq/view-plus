68fc6d24b9ec3b1da30bbeb7849a55538b2bd80e
ccwq <scwqd@qq.com>
2019-11-02 19:48:52 +0800

    go

--- webpack.config.js
+++ webpack.config.js
@@ -69,7 +69,7 @@ module.exports = {
     },
     resolve: {
         alias: {
-            'vue$': 'vue/dist/vue.esm.js'
+            'vue': 'vue/dist/vue.esm.js'
         },
         extensions: ['*', '.js', '.vue', '.json']
     },
