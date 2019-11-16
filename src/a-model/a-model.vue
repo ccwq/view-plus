6a89d0a7c5e0e11537234fca7f66d7550420d086
ccwq <scwqd@qq.com>
2019-11-16 21:06:21 +0800

    a-model slot支持触发回掉

--- src/a-model/a-model.vue
+++ src/a-model/a-model.vue
@@ -5,10 +5,14 @@
         ref="modal"
     )
         slot
-        template(slot="footer")
-            Button(type="warning" @click="cancelHandler" v-if="!noCancel") 取消
-            Button(type="primary" @click="confirmHandler" v-if="!noOk") 确定
-            slot(name="footer")
+        template( slot="footer" )
+            slot(
+                name="footer"
+                :cancel-handler="cancelHandler"
+                :confirm-handler="confirmHandler"
+            )
+                Button(type="warning" @click="cancelHandler" v-if="!noCancel") 取消
+                Button(type="primary" @click="confirmHandler" v-if="!noOk") 确定
 </template>
 <script>
     export default {
