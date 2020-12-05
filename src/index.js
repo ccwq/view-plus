07fc2efe19202c240e0e58faf0f445e6aea652aa
ccwq <scwqd@qq.com>
2020-12-06 01:53:39 +0800

    增加install

--- src/index.js
+++ src/index.js
@@ -1,3 +1,14 @@
+import aTable from "./a-table";
+import coFrom from "./co-form"
 export default {
-
+    install(Vue, options={}){
+        const {} = options;
+        debugger;
+        Vue.mixin({
+            components:{
+                coFrom,
+                aTable,
+            },
+        })
+    }
 }
