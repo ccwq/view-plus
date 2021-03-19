54eacf03d23ed47be02b03e2012cf98e4cac0086
ccwq <scwqd@qq.com>
2021-03-19 14:31:48 +0800

    增加下拉方式选择日期范围模式的功能/并且模式可以前后移动

--- src/a-date-range/a-date-range.vue
+++ src/a-date-range/a-date-range.vue
@@ -1,6 +1,29 @@
 <template lang="pug">
-    .a-date-range-comp.flex
-        slot(name="label" v-if="!hideLabel"): b.pr05 统计时间范围:
+
+    //时间类型选择
+    mixin typeSelect
+        RadioGroup(
+            v-model="date.type"
+            v-show="!hideTypeSelect"
+            v-if="!modeTypeUseSelect"
+        )
+            Radio(:label="a[0]" v-for="a in modeLs" :key="a[0]") {{a[1]}}
+        .dib: a-select(
+            :options="modeLs"
+            v-model="date.type"
+            v-show="!hideTypeSelect"
+            size="small"
+        )
+
+    //- 时间条
+    //- 正式内容
+    .date-filter-bar-comp.flex
+        slot(name="label"): b.pr05 统计时间范围:
+
+        //时间模式
+        template(v-if="modeLsFront")
+            +typeSelect
+            span.w10
         DatePicker(
             :placement="placement"
             transfer
@@ -15,11 +38,13 @@
             @on-change="datePickerValue=$event"
             v-bind="datePickerAttrs"
         )
-        RadioGroup(v-model="date.type" v-show="!hideTypeSelect").ml18
-            Radio(label="daterange") 自由选择
-            Radio(label="year") 年
-            Radio(label="month") 月
-            Radio(label="date") 日
+
+        //- 时间模式
+        template(v-if="!modeLsFront")
+            span.w10
+            +typeSelect
+
+        //- 快速选择
         template(v-if="!hideQuicklySelect").ml-auto
             b 快速选择:
             Button.cl-blue800-i(
@@ -39,16 +64,23 @@
 
 </template>
 <script>
+    import aSelect from "../a-select"
+    import ipro from "ipro";
+    const now = new Date;
 
-import ipro from "ipro";
-const now = new Date;
+    const modeLs = [
+        ["daterange", "自由选择"],
+        ["year", "年"],
+        ["month", "月"],
+        ["date", "日"],
+    ];
 
-const inputWidthByType = {
-    daterange: 200,
-    year:85,
-    month:110,
-    date:120,
-}
+    const inputWidthByType = {
+        daterange: 200,
+        year:85,
+        month:110,
+        date:120,
+    }
 
 export default {
     name: "a-date-range",
@@ -65,6 +97,31 @@ export default {
     },
 
     props:{
+
+
+        /**
+         * 模式切换前置
+         */
+        modeLsFront:{
+            default:false,
+            type:Boolean,
+        },
+
+        /**
+         * 年月日选择使用select
+         */
+        modeTypeUseSelect:{
+            default:false,
+            type:Boolean,
+        },
+
+        /**
+         * 用来隐藏特定的type模式
+         */
+        modeTypeExcluedLs:{
+            default:"",
+            type:[Array, String]
+        },
 
         datePickerAttrs:{
             default:_=>({}),
@@ -154,32 +211,43 @@ export default {
         },
     },
 
-    computed: {
-
-        /**
-         * 当前选择的时间范围
-         * @returns {{start: *, end: *}}
-         */
-        resultDate(){
-            const m = this;
-            const type = m.date.type;
-            const date = m.date.end.clone();
-            let start, end;
-            if (type === "daterange") {
-                start = m.date.start;
-                end = m.date.end;
-            }else if(type == "year"){
-                start = date.clone().setToYearStart();
-                end = date.clone().setToYearEnd();
-            }else if(type == "month"){
-                start = date.clone().setToMonthStart();
-                end = date.clone().setToMonthEnd();
-            }else if(type == "date"){
-                start = date.clone().setToDayStart();
-                end = date.clone().setToDayEnd();
-            }
-            return {start, end};
-        },
+        computed: {
+            modeLs(){
+                const m = this;
+                let typeExLs = m.modeTypeExcluedLs;
+                if (typeof m.modeTypeExcluedLs == "string") {
+                    typeExLs = m.modeTypeExcluedLs.split(",");
+                }
+                const typeExDic = typeExLs.reduce((ret, type) => {
+                    ret[type] = 1;
+                    return ret;
+                }, {});
+                return modeLs.filter(type=>!typeExDic[type[0]])
+            },
+            /**
+             * 当前选择的时间范围
+             * @returns {{start: *, end: *}}
+             */
+            resultDate(){
+                const m = this;
+                const type = m.date.type;
+                const date = m.date.end.clone();
+                let start, end;
+                if (type === "daterange") {
+                    start = m.date.start;
+                    end = m.date.end;
+                }else if(type == "year"){
+                    start = date.clone().setToYearStart();
+                    end = date.clone().setToYearEnd();
+                }else if(type == "month"){
+                    start = date.clone().setToMonthStart();
+                    end = date.clone().setToMonthEnd();
+                }else if(type == "date"){
+                    start = date.clone().setToDayStart();
+                    end = date.clone().setToDayEnd();
+                }
+                return {start, end};
+            },
 
 
         datePickerValue:{
