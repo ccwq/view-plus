540e4298b00435796aba957baf493b7f0ea403dd
ccwq <scwqd@qq.com>
2021-05-11 09:56:25 +0800

    移除对view-design的全局引用

--- src/a-dialog/a-dialog.js
+++ src/a-dialog/a-dialog.js
@@ -1,5 +1,5 @@
-import {Modal} from "view-design";
-
+import isPlainObject from "lodash/isPlainObject"
+let _Modal, _globalTitle;
 /**
  * 确认对话框
  * @param content
@@ -7,13 +7,26 @@ import {Modal} from "view-design";
  * @param cancelText
  * @returns {Promise<boolean>}
  */
-const __confirm = function (rejectMode, content, okText = "确定", cancelText = "取消") {
+const __confirm = function (rejectMode, content, titleOrConfig, okText = "确定", cancelText = "取消") {
+    if (!_Modal) {
+        throw new Error("请先执行aDialogConfig(Modal, title)来初始化a-dialog");
+    }
+    let config, title;
+    if (isPlainObject(titleOrConfig)) {
+        config = titleOrConfig;
+        title = "";
+    }else{
+        title = titleOrConfig;
+        config = {};
+    }
+
     return new Promise((resolve, reject) => {
-        Modal.confirm({
-            title: '请确认',
+        _Modal.confirm({
+            title: title || _globalTitle,
             content,
             okText,
             cancelText,
+            ...config,
             onOk: function () {
                 if (rejectMode) {
                     resolve();
@@ -31,6 +44,16 @@ const __confirm = function (rejectMode, content, okText = "确定", cancelText =
         })
     })
 };
+
+
+/**
+ * 进行Modal初始化
+ * @param $Modal
+ */
+export const aDialogConfig = function(Modal, globalTitle="请确认"){
+    _Modal = Modal;
+    _globalTitle = globalTitle;
+}
 
 export const $confirm = __confirm.bind(null, true);
 export const $confirm2 = __confirm.bind(null, false);
